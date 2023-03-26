cf85a6808032a57f96c7b36c3c77b95c9ce0e6a0
Labasse <labasse@gmail.com>
2023-03-26 14:26:04 +0200

    Afficher des données présentes en mémoire (début)

---
+++ VoyagesFront/Voyage.cs
@@ -0,0 +1,19 @@
+﻿namespace VoyagesFront
+{
+    public class Voyage
+    {
+        public Voyage(string id, string image, string style, string titre, string description)
+        {
+            Id = id;
+            Image = image;
+            Style = style;
+            Titre = titre;
+            Description = description;
+        }
+        public string Id { get; }
+        public string Image { get; }
+        public string Style { get; }
+        public string Titre { get; }
+        public string Description { get; }
+    }
+}
