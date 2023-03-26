b49f16fc64efa93280649656f252a2a6ab45e565
Labasse <labasse@gmail.com>
2023-03-26 14:31:57 +0200

    Synchroniser des données par binding (début)

---
+++ VoyagesFront/Séjour.cs
@@ -0,0 +1,38 @@
+﻿namespace VoyagesFront
+{
+    public class Séjour
+    {
+        private DateTime départ, retour;
+
+        public Séjour(DateTime départ)
+        {
+            this.départ = départ;
+            this.retour = départ + TimeSpan.FromDays(7);
+        }
+        public string Nom { get; set; } = "";
+        public string Prénom => Nom.Split(" ")[0];
+        public uint NbPassagers { get; set; } = 1;
+        public DateTime Départ
+        {
+            get => départ;
+            set
+            {
+                retour = value + Durée;
+                départ = value;
+            }
+        }
+        public DateTime Retour
+        {
+            get => retour;
+            set
+            {
+                if (value <= Départ)
+                {
+                    départ = value - Durée;
+                }
+                retour = value;
+            }
+        }
+        public TimeSpan Durée => Retour - Départ;
+    }
+}
