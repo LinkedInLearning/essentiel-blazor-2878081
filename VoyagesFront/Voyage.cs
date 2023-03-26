f65c0514cb2ce07888364a4198eae1e40664cd6d
Labasse <labasse@gmail.com>
2023-03-26 15:03:54 +0200

    Gérer les évènements (début)

--- VoyagesFront/Voyage.cs
+++ VoyagesFront/Voyage.cs
@@ -2,18 +2,20 @@
 {
     public class Voyage
     {
-        public Voyage(string id, string image, string style, string titre, string description)
+        public Voyage(string id, string image, string style, string titre, string pays, string description)
         {
             Id = id;
             Image = image;
             Style = style;
             Titre = titre;
+            Pays = pays;
             Description = description;
         }
         public string Id { get; }
         public string Image { get; }
         public string Style { get; }
         public string Titre { get; }
+        public string Pays { get; }
         public string Description { get; }
     }
 }
