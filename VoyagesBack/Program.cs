a064291e28f28b64eec85530f4a2c135a7b043c1
Labasse <labasse@gmail.com>
2023-03-27 10:09:39 +0200

    Identifier l'utilisateur (début)

--- VoyagesBack/Program.cs
+++ VoyagesBack/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.EntityFrameworkCore;
+using VoyagesBack.Models;
+using VoyagesBack.Data;
 using Voyages;
 
 namespace VoyagesBack
@@ -11,7 +15,24 @@ namespace VoyagesBack
             // Add services to the container.
             builder.Services.AddScoped<IDataContext>(services => new DataContext() { Voyages = DataContext.DonnéesDeTest });
 
-            builder.Services.AddControllers();
+			// Identity
+			var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString));
+            builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+            builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            builder.Services.AddIdentityServer()
+                .AddApiAuthorization<ApplicationUser, ApplicationDbContext>();
+
+            builder.Services.AddAuthentication()
+                .AddIdentityServerJwt();
+
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddRazorPages();
+            
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
@@ -21,18 +42,29 @@ namespace VoyagesBack
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
+                app.UseMigrationsEndPoint();
+                app.UseWebAssemblyDebugging();
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
-
+			else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
             app.UseHttpsRedirection();
 
             app.UseBlazorFrameworkFiles();
             app.UseStaticFiles();
 
+            app.UseRouting();
+
+            app.UseIdentityServer();
             app.UseAuthorization();
 
 
+            app.MapRazorPages();
             app.MapControllers();
             app.MapFallbackToFile("index.html");
 
