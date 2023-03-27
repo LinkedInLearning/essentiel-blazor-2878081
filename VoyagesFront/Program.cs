aafef10fd98bd54b1a923f9302f17e9324bd51f6
Labasse <labasse@gmail.com>
2023-03-27 11:15:16 +0200

    Sécuriser avec des autorisations (fin)

--- VoyagesFront/Program.cs
+++ VoyagesFront/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Voyages;
 
@@ -6,6 +7,9 @@ namespace VoyagesFront
 {
     public class Program
     {
+        public const string HttpClientPublic = "VoyagesAPIPublic";
+        public const string HttpClientPrive = "VoyagesAPIPrive";
+
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -13,6 +17,10 @@ namespace VoyagesFront
             builder.RootComponents.Add<HeadOutlet>("head::after");
 
             builder.Services.AddApiAuthorization();
+            builder.Services.AddHttpClient(HttpClientPublic, client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
+            builder.Services.AddHttpClient(HttpClientPrive , client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+                .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+            builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient(HttpClientPublic));
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
             builder.Services.AddSingleton<IDataContext>(s => new DataContext());
