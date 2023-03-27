14f9fd56600dc6d3a506cb3c79e05012138fd1e9
Labasse <labasse@gmail.com>
2023-03-27 10:57:17 +0200

    Sécuriser avec des autorisations (début)

--- VoyagesBack/Controllers/SejoursController.cs
+++ VoyagesBack/Controllers/SejoursController.cs
@@ -1,8 +1,10 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Voyages;
 
 namespace VoyagesBack.Controllers
 {
+    [Authorize]
     [ApiController]
     [Route("api/[controller]")]
     public class SejoursController : ControllerBase
