92cf73690ec3be90d95c96963fc7b32c618e10fd
Labasse <labasse@gmail.com>
2024-06-05 00:32:57 +0200

    Porter sur mobile natif (début)

---
+++ ExempleMobile/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ExempleMobile
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ExempleMobile/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ExempleMobile
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
